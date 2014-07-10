7401375d41da11650f2a2ee2b47be3b8774e52aa
Matt <matt16749@gmail.com>
2014-07-10 08:45:42 -0700

    initial commit cloud

---
+++ blog-posts/scripts/blog_template.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+function newPos() {
+   var w = $(window).width();
+   var nw = Math.floor(Math.random() * w);
+    
+   var nh = Math.floor(Math.random() * 30)+'%';
+    
+   return [nw, nh];
+}
+
+function animateCloud(image){
+   $(image).animate({        
+        top: newPos()[1],
+        left: newPos()[0]
+   }, 3000, function(){
+       animateCloud(image);
+   });
+}   
+
+animateCloud($(".cloud"));
+
+});
