61a67ada2cadc323241b07ff4d18697223e8bf68
Matt <matt16749@gmail.com>
2014-07-10 09:59:59 -0700

    added link jquery

--- blog-posts/scripts/blog_template.js
+++ blog-posts/scripts/blog_template.js
@@ -1,29 +1,37 @@
 $(document).ready(function(){
-$(window).load(function(){
+
+//local links are loaded on same window, foreign links are 
+//loaded in new window
+$("a").attr("target", function() {
+if (this.host == location.host) return "_self"
+else return "_blank"; });﻿
 
 
+//delay script until window loads.
+  $(window).load(function(){
 
-function newPos() {
-   var w = $(window).width();
-   var nw = Math.floor(Math.random() * w);
-    
-   var nh = Math.floor(Math.random() * 28)+'%';
-    
-   return [nw, nh];
-}
+  //for cloud animation
+    function newPos() {
+       var w = $(window).width();
+       var nw = Math.floor(Math.random() * w);
+        
+       var nh = Math.floor(Math.random() * 28)+'%';
+        
+       return [nw, nh];
+    }
 
-function animateCloud(image){
-   $(image).animate({        
-        top: newPos()[1],
-        left: newPos()[0]
-   }, 20000, function(){
-       animateCloud(image);
-   });
-}   
+    function animateCloud(image){
+       $(image).animate({        
+            top: newPos()[1],
+            left: newPos()[0]
+       }, 20000, function(){
+           animateCloud(image);
+       });
+    }   
 
-animateCloud($("#cloud0"));
-animateCloud($("#cloud1"));
-animateCloud($("#cloud2"));
+    animateCloud($("#cloud0"));
+    animateCloud($("#cloud1"));
+    animateCloud($("#cloud2"));
 
-});
+  });
 });
