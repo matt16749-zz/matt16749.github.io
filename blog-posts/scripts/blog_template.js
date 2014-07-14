113af3b3d002985b3b63bcca3102edfb061e468e
Matt <matt16749@gmail.com>
2014-07-13 18:38:11 -0700

    playing with jquery

--- blog-posts/scripts/blog_template.js
+++ blog-posts/scripts/blog_template.js
@@ -20,7 +20,7 @@ else return "_blank"; });﻿
        $(image).stop(true).animate({        
             top: newPos()[1],
             left: newPos()[0]
-       }, 20000, function(){
+       }, 3000, function(){
            animateCloud(image);
        });
     }   
