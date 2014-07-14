a41e159324de997f19c4382e21719a7ee2b01fb9
Matt <matt16749@gmail.com>
2014-07-13 18:37:41 -0700

    made clouds faster?

--- blog-posts/scripts/blog_template.js
+++ blog-posts/scripts/blog_template.js
@@ -20,7 +20,7 @@ else return "_blank"; });﻿
        $(image).stop(true).animate({        
             top: newPos()[1],
             left: newPos()[0]
-       }, 40000, function(){
+       }, 20000, function(){
            animateCloud(image);
        });
     }   
