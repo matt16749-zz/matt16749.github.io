5b380e73b4bdc3857641401f88cf43bcb837458c
Matt <matt16749@gmail.com>
2014-07-13 18:33:39 -0700

    changing timing to random

--- blog-posts/scripts/blog_template.js
+++ blog-posts/scripts/blog_template.js
@@ -20,7 +20,7 @@ else return "_blank"; });﻿
        $(image).stop(true).animate({        
             top: newPos()[1],
             left: newPos()[0]
-       }, 20000, function(){
+       }, Math.floor(Math.random() * 30000) + 15000, function(){
            animateCloud(image);
        });
     }   
